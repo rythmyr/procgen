eb9c85bde10a0e6e9f497c2b4ce1a80db9ce4508
Rythmyr <rythmyr@gmail.com>
2021-03-06 23:25:26 -0600

    add dependencies, create perlin noise component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { PerlinNoiseComponent } from './perlin-noise/perlin-noise.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PerlinNoiseComponent
   ],
   imports: [
+    MatSliderModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/perlin-noise/perlin-noise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'pg-perlin-noise',
+  templateUrl: './perlin-noise.component.html',
+  styleUrls: ['./perlin-noise.component.scss']
+})
+export class PerlinNoiseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
